390afac987377f7a33b64e9c4e17ac58056a78f6
shahshad <www.shahshadbabu.com>
2023-10-24 20:55:47 +0530

    update them

--- controllers/driver.go
+++ controllers/driver.go
@@ -27,37 +27,70 @@ func CreateDriver(c *gin.Context) {
 	}
 
 	if err := database.DB.Create(&models.Driver{
-		Name: driver.Name,
-		LastName: driver.LastName,
-		PhoneNumber: driver.PhoneNumber,
-		Email: driver.Email,
-		BirthDate: driver.BirthDate,
-		DriverImg:driver.DriverImg,
-        Gender: driver.Gender,
+		Name:           driver.Name,
+		LastName:       driver.LastName,
+		PhoneNumber:    driver.PhoneNumber,
+		Email:          driver.Email,
+		BirthDate:      driver.BirthDate,
+		DriverImg:      driver.DriverImg,
+		Gender:         driver.Gender,
 		Qualifications: driver.Qualifications,
-		Expirience: driver.Expirience,
+		Expirience:     driver.Expirience,
 	}).Error; err != nil {
 		c.JSON(500, gin.H{
-			"error": "failed to add detailes in database",
+			"error": "failed to add DriverDetails in database",
 		})
 		return
 	}
+
+	if err := database.DB.Create(&models.VehicleDetails{
+		DriverId:  driver.ID,
+		CarBrand:  driver.CarBrand,
+		CarModel:  driver.CarModel,
+		CarYear:   driver.CarYear,
+		CarColor:  driver.CarColor,
+		CarSeat:   driver.CarSeat,
+		CarNumber: driver.CarNumber,
+	}).Error; err != nil {
+		c.JSON(500, gin.H{
+			"error": "failed to add VehicleDetails in database",
+		})
+		return
+	}
+
+	if err := database.DB.Create(&models.DriverDocument{
+		DriverId:     driver.ID,
+		LicenseNo:    driver.LicenseNo,
+		LicenceExp:   driver.LicenceExp,
+		LicenceFront: driver.LicenceFront,
+		LicenceBack:  driver.LicenceBack,
+		AdharNo:      driver.AdharNo,
+		AdharAddress: driver.AdharAddress,
+		AdharFront:   driver.AdharFront,
+		AdharBack:    driver.AdharBack,
+	}).Error; err != nil {
+		c.JSON(500, gin.H{
+			"error": "failed to add DriverDocumets in database",
+		})
+		return
+	}
+
 	c.JSON(200, gin.H{
 		"success": "successfully created driver" + driver.Name,
 	})
 }
 
 func GetDriverDetail(c *gin.Context) {
-	id,_ := strconv.Atoi(c.Param("driver_id"))
+	id, _ := strconv.Atoi(c.Param("driver_id"))
 	var driver models.Driver
-	if err := database.DB.First(&driver,id).Error;err != nil {
-		c.JSON(400,gin.H{
-			"error":"failed to find user",
+	if err := database.DB.First(&driver, id).Error; err != nil {
+		c.JSON(400, gin.H{
+			"error": "failed to find user",
 		})
 		return
 	}
-	
-	c.JSON(200,gin.H{
-		"driver":driver,
+
+	c.JSON(200, gin.H{
+		"driver": driver,
 	})
 }
